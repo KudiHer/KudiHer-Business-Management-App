51be9df98f285888990ccae155f0001f27b497be
Dolly Chepkorir <dollychepkorir@gmail.com>
2026-02-20 11:15:55 +0300

    feat(backend): add auth login flow and fix server startup paths

--- src/models/User.js
+++ src/models/User.js
@@ -44,10 +44,14 @@ const userSchema = new mongoose.Schema({
 // Encrypt password using bcrypt before saving to database
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
-    next();
+    return next();
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
