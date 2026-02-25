dca5e9aa1369dbf75ededfd86151ceaa2b388294
Ladunnitegbe <akinsolaladunni@gmail.com>
2026-02-25 11:24:27 +0100

    Completed dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,15 @@
 import './themes/global.css';
 
 import { Routes, Route } from "react-router-dom";
-import { SideBar } from "./components/SideBar/SideBar";
-import { Dashboard } from "./pages/Dashboard/Dashboard";
+
+import  Dashboard   from "./pages/Dashboard/Dashboard";
 
 
 function App() {
    return (
     <>
     <main className="appContainer">
-    <SideBar />
+    
 
     <Routes>
       <Route path="/" element={<Dashboard />} />
